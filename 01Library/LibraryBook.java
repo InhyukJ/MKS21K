949c262cdb6683f110cf0bcb6346e7cc612ec020
InhyukJ <ijun@stuy.edu>
2016-12-04 21:46:33 -0500

    Needs some tweaks

---
+++ 01Library/LibraryBook.java
@@ -0,0 +1,33 @@
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook> {
+    private String callNumber;
+    public LibraryBook (String author, String title, String ISBN, String callNumber) {
+        super(author,title,ISBN);
+	callNumber = callnumber;
+    }
+
+    public String getCallNumber() {
+	String x = "";
+	x += callNumber;
+	return x;
+    }
+
+     public void setCallNumber(String callnumber) {
+	callNumber = callnumber;
+    }
+
+    abstract void checkout(String patron, String due);
+    
+    abstract void returned();
+
+    abstract String circulationStatus();
+
+    public int compareTo(LibraryBook o) {
+	return callNumber.compareTo(o.getCallNumber());
+    }
+    
+
+    public String toString() {
+	String a = super.toString();
+	return a + ", " + circulationStatus() + ", " + callNumber;
+    }
+}
