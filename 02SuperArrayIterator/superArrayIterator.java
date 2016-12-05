a6cfbf2f54c7903a447bb4b601a76d4cbd19c96e
InhyukJ <ijun@stuy.edu>
2016-12-04 21:57:48 -0500

    Does not work?

--- 02SuperArrayIterator/superArrayIterator.java
+++ 02SuperArrayIterator/superArrayIterator.java
@@ -1,12 +1,27 @@
 import java.util.Iterator;
 
-public class superArrayIterator extends superArray implements Iterator<String> {
-    superArray data;
-    int size;
+public class superArrayIterator implements Iterator<String>  {
 
-    public superArrayIterator() {
-	super();
-    }	
+    private superArray ary;
+    private int elementToLook;
 
+    public superArrayIterator (superArray array){
+	ary  = array;
+	elementToLook = 0;
+    }
+    
     public boolean hasNext() {
+	return elementToLook < ary.size();
+    }
+
+    public String next() {
+	elementToLook++;
+	return ary.get(elementToLook - 1);
 	
+    }
+
+    public void remove() {
+	throw new UnsupportedOperationException();
+    }
+
+}
