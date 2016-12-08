bb33bbc3587bf396abb13e2fe0552a4c88e6be84
InhyukJ <ijun@stuy.edu>
2016-12-07 21:38:41 -0500

    almost done

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -25,21 +25,55 @@ public class Barcode implements Comparable<Barcode>{
   }
 
 // postcondition: Creates a copy of a bar code.
-  public Barcode clone(){}
+  public Barcode clone(){
+      Barcode nBarcode = new Barcode(_zip);
+      return nBarcode;
+  }
 
 
 // postcondition: computes and returns the check sum for _zip
   private int checkSum(){
-      int zipInt = Integer.parseInt(zip);
-      
+      int sumZip = 0;
+      int num = Integer.parseInt(zip);
+      while (num > 0) {
+	  sumZip += num % 10;
+	  num /= 10;
+      }
+      return sumZip;
   }
 
+  private int returnDigit(int n) {
+      String zip = _zip;
+      return zip.charAt(n);
+  }
+      
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-  public String toString(){}
+  public String toString(){
+      String[]reference = new String[10];
+      reference.add("||:::"); //0
+      reference.add(":::||"); //1
+      reference.add("::|:|"); //2
+      reference.add("::||:"); //3
+      reference.add(":|::|"); //4
+      reference.add(":|:|:"); //5
+      reference.add(":||::"); //6
+      reference.add("|:::|"); //7
+      reference.add("|::|:"); //8
+      reference.add("|:|::"); //9
+
+      String ans = "The bar code of " + _zip + " with they check digit of " + _checkDigit + " is: ";
+      String sAns = "|";
+      for (int n = 0;n > 5;n++) {
+	  sAns += reference[returnDigit(n)];
+      }
+      return ans + sAns + "|";
+  }
 
 
 // postcondition: compares the zip + checkdigit, in numerical order. 
-  public int compareTo(Barcode other){}
+  public int compareTo(Barcode other){
+      //idgi
+  }
     
 }
